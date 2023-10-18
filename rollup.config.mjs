248b9c7abfc4533b5de5fd4b4a8a6fc5691f28c9
Jung gyu cheol <kcpooh0202@naver.com>
2023-10-18 16:22:30 +0900

    Fix: rollup.config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,17 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import url from '@rollup/plugin-url'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json' assert { type: 'json' }
-import * as react from 'react'
 
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: 'cjs', sourcemap: true, interop: 'compat' },
-    { file: pkg.module, format: 'es', sourcemap: true, interop: 'compat' },
+    { file: pkg.main, format: 'cjs', interop: 'compat' },
+    { file: pkg.module, format: 'es', interop: 'compat' },
   ],
+  external: ['react', 'react-collapsed', 'styled-components', 'styled-system'],
   plugins: [url(), typescript(), commonjs(), terser()],
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/accordion'
 export * from './components/Box'
 export * from './components/Text'
+export * from './components/Dot'
 export * from './theme'
